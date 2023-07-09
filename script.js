15a42686bddc04cf73c2eb1e15953b02c5c7010d
DevCephas <peterolanrewaju01@gmail.com>
2023-07-09 11:28:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let userLocation = document.querySelector('#userLocation');
+let myLocation = document.querySelector('#myLocation');
+
+myLocation.addEventListener("click", () => {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(geoLocation);
+  }else {
+    userLocation.innerText = "The geolocation is not supported in this browser";
+  }
+});
+
+const geoLocation = async (position) => {
+  let response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
+  );
+  let  data = await response.json();
+  console.log(data)
+  userLocation.innerText = `${data.address.state_district}, ${data.address.state}, ${data.address.country}`;
+};
